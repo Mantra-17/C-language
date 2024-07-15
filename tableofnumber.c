f694ce03280d775a92bcb2b884cdfa5a7321ea1d
Mantra-17 <mantrapatel46@gmail.com>
2024-07-15 23:20:38 +0530

    arithmetic operations

--- tableofnumber.c
+++ tableofnumber.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
 
 int main() {
-    int n;
-//multiplication table of any number n
-    printf("enter the number :");
-    scanf("%d", &n);
-
-    for(int i=1; i<=10; i++)
-    {
-        printf("%d*%d=%d\n", n,i,n*i);
-    }
+   int i = 1;
+   do {
+    printf("%d\n", i);
+    i++;
+    while(1<=10);
+   }
 }
