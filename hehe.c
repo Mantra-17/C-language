4b47e2f5fda0e6a0d55b20e339a877869266a695
Mantra-17 <mantrapatel46@gmail.com>
2024-07-16 23:16:25 +0530

    function in c

---
+++ hehe.c
@@ -0,0 +1,18 @@
+ #include<stdio.h>
+ // Declaration or [prototype]
+ void printHello();
+ void printgoodbye();
+
+ int main() {
+    printHello();//call function
+    printgoodbye();
+    return 0;
+ }
+// function defination
+ void printHello() {
+    printf("Hello World!\n ");
+    printf("My name is Mantra Patel\n");
+ }
+ void printgoodbye() {
+    printf("Goodbye:)\n");
+ }
