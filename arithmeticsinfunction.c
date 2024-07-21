782640a15686d69403f41c6ae0ea666236943141
Mantra-17 <mantrapatel46@gmail.com>
2024-07-21 10:06:36 +0530

    square of numbers using functions

--- arithmeticsinfunction.c
+++ arithmeticsinfunction.c
@@ -1,23 +1,2 @@
 #include<stdio.h>
 
-void printarithmetic();
-int a;
-int b;
-int main() {
-    // arithmetic using functions
-
-    printf("enter the value of a:");
-    scanf("%d", &a);
-
-    printf("enter the value of b:");
-    scanf("%d", &b);
-
-     printarithmetic();
-    return 0;
-
-}
-
-void printarithmetic() {
-
-    printf("sum of two numbers is : %d\n", a+b);
-}
