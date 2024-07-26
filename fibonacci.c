78e12e378b400d13fefd85a99d335eb3c09e03a8
Mantra-17 <mantrapatel46@gmail.com>
2024-07-26 22:20:19 +0530

    square root of any number

---
+++ fibonacci.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+
+    printf("enter n :");
+    scanf("%d", &n);
+
+    printf("square root of a number is : %d", sqrt(n));
+    
+
+    return 0;
+}
+
